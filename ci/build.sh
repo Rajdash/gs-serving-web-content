b27db656c5caa46c0b694b9f6fb8b156ac0e5b66
Rajdash <rajranjandash@gmail.com>
2017-11-29 10:14:35 +0000

    added

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 cd source-code
-  ./complete/mvnw clean package
+apache-maven-3.2.1/bin/mvn clean package -f complete/pom.xml 
 cd ..
 
 cp source-code/complete/target/gs-serving-web-content-1.4-SNAPSHOT.jar  build-output/.
